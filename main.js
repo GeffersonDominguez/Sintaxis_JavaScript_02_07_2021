000b4d35b3159b2d14fc3afad37cde1f23c4e9ce
GeffersonDominguez <geffersondominguez93@gmail.com>
2021-07-02 21:52:07 -0500

    Variable Tipos de variables y consolas

---
+++ main.js
@@ -0,0 +1,115 @@
+
+
+nombreString = `Gefferson Dominguez`
+edadNumber=28;
+hobbiesArray = `cocinar`
+console.log(hobbiesArray)
+hobbiesArray = ["Programar","Cocino", 3043640755, null];
+hobbiesArray["Soy profesor"] = soyProfesorBolena;
+dirrecionPersonalObjecte = {
+    dirrecion: "Calle 11",
+    pais: "Colombia",
+    Pais: "Colombia2",
+    mundo: "Tierra",
+    0: "Hola soy el identificador numerico"
+};
+
+console.log(hobbiesArray);
+console.log(dirrecionPersonalObjecte);
+console.log(`Datos de la variable tipo Array: `,hobbiesArray["Soy profesor"], hobbiesArray[0]);
+console.log(`Datos de la variable tipo Object: `,dirrecionPersonalObjecte.pais, dirrecionPersonalObjecte[0]);
+
+
+
+
+
+
+
+
+
+
+
+// num1 = 50;
+// num2 = 3;
+
+// tipoDeDato = typeof(num1);
+
+
+// console.log(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+
+
+
+
+
+
+// console.log(`%c
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `,"color:orange; font-family:cursive;");
+
+// console.log(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+// console.error(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+// console.info(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+// console.warn(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+// document.write(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+// alert(`
+//     Numero 1 = ${num1} 
+//     Numero 2 = ${num2}
+//     Resultado de la suma = ${num1 + num2}
+// `);
+
+
+
+
+
+
+
+
+// console.log("El resultado de la suma es: "+suma);
+// console.log('El resultado de la suma es: '+suma);
+// console.log('El resultado de la suma es: ',suma);
+
+
+
+
+
+
+
+// nombreCompleto = undefined;
+// console.log(nombreCompleto);
+// num1=50;
+// num2=3;
+// suma=num1+num2;
+// console.log(`
+// Numero 1= ${num1}
+// Numero 2= ${num2}
+// Resultado de la suma = ${num1 + num2}`
+// );
